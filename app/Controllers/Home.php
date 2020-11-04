5f163c1f6408f749e170e0535ed5593ccdad4ff4
karimatussofiah <58159949+karimatussofiah@users.noreply.github.com>
2020-11-04 13:58:38 +0700

    template admin

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,16 +1,44 @@
-<?php namespace App\Controllers;
+<?php 
+
+namespace App\Controllers;
+
+use App\Models\Modeldashboard;
 
 class Home extends BaseController
 {
-  public function index()
+  protected $Modeldashboard;
+
+  public function __Construct()
+
   {
-    $data =[
-      'title' => 'Home',
-      'content' => 'layout/v_wrapper',
-    ];
-    echo view('layout/v_wrapper', $data);
+    $this->Modeldashboard = new Modeldashboard();
   }
 
-  //--------------------------------------------------------------------
 
+  public function index()
+  {
+        $session = session();
+        echo "Welcome back, ".$session->get('user_email');
+    $data = [
+      'judul' => 'Tampilan Admin'
+    ];
+
+    echo view('template/v_header', $data);
+    echo view('template/v_sidebar');
+    echo view('template/v_topbar');
+    echo view('home/index');
+    echo view('template/v_footer');
+  }
+
+  public function dashboard()
+  {
+    $Home = $this->Modeldashboard->findAll();
+
+    $data = [
+      'judul' => 'Dashboard',
+      'home' => $Home,
+    ];
+
+    echo view('dashboard', $data);
+  }
 }
---
+++ app/Views/template/v_sidebar.php
@@ -0,0 +1,85 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <div class="sidebar-brand-icon rotate-n-15">
+  </div>
+  <div class="sidebar-brand-text mx-3">SMART CITY</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+  <a class="nav-link" href="<?= base_url('/home/dashboard'); ?>">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    <span>Dashboard</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Member
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+  <a class="nav-link collapsed" href="<?= base_url('kategori'); ?>"> 
+    <i class="fas fa-fw fa-table"></i>
+    <span>Data Kategori</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <h6 class="collapse-header">Custom Components:</h6>
+      <a class="collapse-item" href="buttons.html">Buttons</a>
+      <a class="collapse-item" href="cards.html">Cards</a>
+    </div>
+  </div>
+</li>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<!--<li class="nav-item">
+  <a class="nav-link collapsed" href="<?= base_url('blog'); ?>"> 
+    <i class="fas fa-fw fa-table"></i>
+    <span>Data Modal</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <h6 class="collapse-header">Custom Components:</h6>
+      <a class="collapse-item" href="buttons.html">Buttons</a>
+      <a class="collapse-item" href="cards.html">Cards</a>
+    </div>
+  </div>
+</li> -->
+
+<!-- Nav Item - Pages Collapse Menu -->
+<!--<li class="nav-item">
+  <a class="nav-link collapsed" href="<?= base_url('status'); ?>"> 
+    <i class="fas fa-fw fa-table"></i>
+    <span>Data Status</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <h6 class="collapse-header">Custom Components:</h6>
+      <a class="collapse-item" href="buttons.html">Buttons</a>
+      <a class="collapse-item" href="cards.html">Cards</a>
+    </div>
+  </div>
+</li> -->
+
+
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
