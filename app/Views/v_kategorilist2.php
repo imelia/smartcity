4d66151576f7157d4594b254903abc45961f5c19
wellananda <wellananda01@gmail.com>
2020-11-04 15:23:13 +0700

    newwwww

--- app/Views/v_kategorilist2.php
+++ app/Views/v_kategorilist2.php
@@ -62,6 +62,11 @@
     <i class="fas fa-fw fa-table"></i>
     <span>Data Kategori</span>
   </a>
+  <li class="nav-item">
+  <a class="nav-link collapsed" href="<?= base_url('modal'); ?>"> 
+    <i class="fas fa-fw fa-table"></i>
+    <span>Data Modal</span>
+  </a>
   <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
       <h6 class="collapse-header">Custom Components:</h6>
