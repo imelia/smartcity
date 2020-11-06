a398d1a8b81dbb5c3cd5e1affaad2b913f5a68bb
imelia <58154068+imelia@users.noreply.github.com>
2020-11-06 09:28:09 +0700

    Update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,25 +17,11 @@ class Home extends BaseController
 
   public function index()
   {
-<<<<<<< Updated upstream
-        $session = session();
-        echo "Welcome back, ".$session->get('user_email');
-    $data = [
-      'judul' => 'Tampilan Admin'
-    ];
-
-    echo view('template/v_header', $data);
-    echo view('template/v_sidebar');
-    echo view('template/v_topbar');
-    echo view('home/index');
-    echo view('template/v_footer');
-=======
     $data =[
       'title' => 'Home',
       'content' => 'situbondo',
     ];
     echo view('situbondo', $data);
->>>>>>> Stashed changes
   }
 
   public function dashboard()
